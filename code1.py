265c3d11f0f6ec420d7830310bde4f606b2f64a9
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-21 12:41:41 +0530

    Added Code File

---
+++ code1.py
@@ -0,0 +1,13 @@
+'''
+Rishu has to buy 1000 mangoes amd cost of each mangoes is 20 Rs.But Rishu has only 10 Rs.He wants to buy 5 mangoes.
+how much should rishu need more to buy 5 mangoes ?
+
+Author : Noor
+Date:21/10/23
+
+'''
+cost_per_unit_mangoes=20
+money_rishu_has=10
+Price_five_Mangoes=5*cost_per_unit_mangoes
+money_rishu_needed=Price_five_Mangoes - money_rishu_has
+print('money rishu needed to buy 5 mangoes :',money_rishu_needed,'Rs')
