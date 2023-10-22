3a7b5e655c3203598cf82989903a6a1b363db80d
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-22 11:06:59 +0530

    Temperature Converter

---
+++ code3.py
@@ -0,0 +1,14 @@
+'''
+Write a program which prompts user input for a celcius temperature,convert this temperature into foreinheight
+and print this converted temperature.
+
+Author:Noor
+Date:22/10/23
+'''
+
+Temp=int(input('Temperature in degree celcius: '))
+print(Temp * 9/5 + 32,'degree farenheight')        
+         
+         
+    
+         
