4dc2ba1420c61b961b7bee561c94999227bf7e15
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-22 11:40:55 +0530

    Class Type

---
+++ code5.py
@@ -0,0 +1,23 @@
+'''
+Assume the following assignment statements
+w=17
+h=12.0
+for each of the following expressions,write the value of expression and its type.
+w/2               8.5     float
+w/2.0             8.5     float
+h/3               4.0     float
+1+2*5             11      int
+
+Author:Noor
+Date:22/10/23
+'''
+
+w=17
+h=12.0
+print('w/2=',w/2,'\ttype(w/2)=',type(w/2))
+print('w/2.0=',w/2.0,'\ttype(w/2.0)=',type(w/2.0))
+print('h/3=',h/3,'\ttype(h/3)=',type(h/3))
+print('1+2*5=',1+2*5,'\ttype(1+2*5)=',type(1+2*5))
+
+
+
