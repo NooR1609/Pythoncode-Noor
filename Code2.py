04dc94f9923c074ce97f3b2200340fec9b81c232
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-22 10:46:56 +0530

    Delete Code2.py

--- Code2.py
+++
@@ -1,9 +0,0 @@
-'''
-write a program that uses input to prompt a user for their name and then welcomes them 
-
-Author:Noor
-Date-22/10/23
-'''
-
-name=input('Enter Guest Name: ')
-print('Hello',name,'\nYou are welcome!')
