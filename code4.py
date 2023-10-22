7602c95c1016d6b60dcfae8bab9ff77c741b7e12
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-22 11:38:38 +0530

    Mathematical expression

---
+++ code4.py
@@ -0,0 +1,13 @@
+'''
+Write a program to prompt a user for hours and rate per hour to compute gross pay
+
+Author:Noor
+Date:22/10/23
+'''
+
+working_hours=int(input('Enter your working Hour: '))
+rate_per_hour=int(input('Rate per Hour?: '))
+gross_pay= working_hours* rate_per_hour
+print('Your Gross Pay is:',gross_pay)
+
+                  
