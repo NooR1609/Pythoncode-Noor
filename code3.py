da649fa9a475d2acce2fb6d66c32291cb44562bc
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-10-26 17:52:05 +0530

    temperature conversion

--- code3.py
+++ code3.py
@@ -8,7 +8,7 @@ Date:22/10/23
 
 Temp=int(input('Temperature in degree celcius: '))
 temp_in_farenheit=Temp * 9/5 + 32
-print('Temperature in degree farenheit is:',temp_in_farenheit)        
+print('Temperature in degree farenheit:',temp_in_farenheit)        
          
          
     
