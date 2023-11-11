1a220d87f5f322a94ed18c85adbbd638576c7d0a
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-11-11 20:56:33 +0530

    added a new code

---
+++ code8.py
@@ -0,0 +1,14 @@
+'''
+Write a code that prints true only if given two user input numbers are positive.
+
+Author:Noor
+Date:11/11/23
+'''
+
+num1=int(input('enter a number:'))
+num2=int(input('enter another number:'))
+if(num1>0 and num2>0):
+    print('True.')
+else:
+    print('False.')
+    
