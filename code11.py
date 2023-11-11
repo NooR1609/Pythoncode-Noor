2f953d727d1f3253ccc7f41e7213df431df0ce86
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-11-11 22:04:07 +0530

    added new file

---
+++ code11.py
@@ -0,0 +1,21 @@
+'''
+Write a program to check that a given is odd, even or zero. Number should be user input.
+
+Author:Noor
+Date:11/11/23
+'''
+
+num=int(input('enter a number:'))
+
+print('Given number is ',end='')
+if (num==0):
+    print('zero')
+elif(num%2==0):
+    print('even')
+else:
+    print('odd')
+    
+    
+    
+    
+    
