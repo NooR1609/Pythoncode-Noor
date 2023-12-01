24696c8bdb4ca7a195cfd54953ab986bac75a68c
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-12-01 22:04:28 +0530

    updated code13.py

--- code13.py
+++ code13.py
@@ -9,11 +9,9 @@ Date:12/11/23
 a1=int(input('Enter 1st angle:'))
 a2=int(input('Enter 2nd angle:'))
 a3=int(input('Enter 3rd angle:'))
-if(a1>0 and a2>0 and a3>0):
-    if (a1+a2+a3==180):
-         print('Triangle is valid with',a1,a2,'and',a3,'.')
-    else:
-        print('Triangle is not valid with',a1,a2,'and',a3,'.')
+if(a1>0 and a2>0 and a3>0) and (a1+a2+a3==180):
+    print('Triangle is valid with',a1,a2,'and',a3,'.')
+   
 else:
     print('Triangle is not valid with',a1,a2,'and',a3,'.')
     
