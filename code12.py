5b3758401cd224b1c87cc1e1f23f61a4dcc18d67
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-12-01 21:58:50 +0530

    updated code12.py

--- code12.py
+++ code12.py
@@ -15,9 +15,7 @@ Date:12/11/23
 '''
 
 year=int(input('Enter the year:'))
-if (year%400==0):
-    print(year,'is a leap year.')
-elif (year%100!=0 and year%4==0):
+if (year%400==0)or(year%100!=0 and year%4==0):
     print(year,'is a leap year.')
 else:
     print(year,'is not a leap year.')
