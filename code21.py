addca6ee375e99a481b20b347a44c07854d90ed3
NooR1609 <148611716+NooR1609@users.noreply.github.com>
2023-12-04 15:45:21 +0530

    updated code file

--- code21.py
+++ code21.py
@@ -7,18 +7,16 @@ Date:02/12/23
 start=int(input('Enter start range:'))
 end=int(input('Enter end range:'))
 
-prime_counter=0
-
 for i in range(start,end+1):
-    num=i
-    is_prime=True
-    for p in range(2,num):
-        if(num%p==0):
-            is_prime=False
-    if(is_prime and (num!=1)):
-        prime_counter=prime_counter+1
+    if(i>1):
+        for p in range(2,i):
+            if(i%p==0):
+                break
+        else:
+            print('Prime numbers in range',start,',',end,'=',i)
 
-print('Prime numbers in range',start,',',end,'=',prime_counter)
+
+
             
     
     
